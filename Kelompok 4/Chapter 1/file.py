9632e86f434e1f9d8b306b4276b51dc4f1ef0578
BryanFlava <bray.gaul99@gmail.com>
2023-02-24 03:06:43 +0700

    C1-1204041-Bryan Samperura

--- Kelompok 4/Chapter 1/file.py
+++ Kelompok 4/Chapter 1/file.py
@@ -1,10 +1,10 @@
-f = open ('coba.txt', 'w')
+f = open ('Kelompok 4/Chapter 1/coba.txt', 'w')
 f.write ('Kapibara From Kelompok 4 \n') 
 
 f.write ('Kapibara Suka Berenang \n') 
 
 f.close()
-f = open ('coba.txt')
+f = open ('Kelompok 4/Chapter 1/coba.txt')
 content = f.read()
 print (content)
 
