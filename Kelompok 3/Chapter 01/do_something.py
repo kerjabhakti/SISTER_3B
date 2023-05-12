41c920dd222ef5da2f825a9ea4e4480e4040b162
BryanFlava <bray.gaul99@gmail.com>
2023-05-12 11:52:40 +0700

    C1-3B

--- Kelompok 3/Chapter 01/do_something.py
+++
@@ -1,9 +0,0 @@
-import random
-# Melakukan perhitungan dasar, termasuk membangun sebuah daftar bilangan bulat secara acak. 
-# Fungsi ini akan digunakan dalam sebuah program Python yang bertujuan untuk membandingkan waktu eksekusi 
-# dari tiga skenario yaitu serial, multithread, dan multiprocess.
-
-def do_something(count,out_list):
-	for i in range(count):
-		out_list.append(random.random())
-
--- Kelompok 5/Chapter 01/do_something.py
+++
@@ -1,5 +0,0 @@
-import random
-
-def do_something(count,out_list):
-	for i in range(count):
-		out_list.append(random.random())
